b13345f3e23b2b08ef60e720a72ea9a9fa63d2a5
Silvio Tomatis <silviot@gmail.com>
2012-03-07 01:33:39 +0100

    a bit of refactoring and a couple of hacks to push codemirror inside Plone (Plomino)

---
+++ src/collective/codemirror/static/convertTextAreas.js
@@ -0,0 +1,86 @@
+if (document.getElementsByClassName == undefined) {
+  document.getElementsByClassName = function(className)
+  {
+    var hasClassName = new RegExp("(?:^|\\s)" + className + "(?:$|\\s)");
+    var allElements = document.getElementsByTagName("*");
+    var results = [];
+
+    var element;
+    for (var i = 0; (element = allElements[i]) != null; i++) {
+      var elementClass = element.className;
+      if (elementClass && elementClass.indexOf(className) != -1 && hasClassName.test(elementClass))
+        results.push(element);
+    }
+
+    return results;
+  }
+}
+
+(function() {
+  var convert_textarea = function(area){
+    var cm = CodeMirror.fromTextArea(area, {
+      value: area.value,
+      mode: 'python',
+      lineNumbers: true
+    });
+    if (area.onCodeMirrorLoad) {
+      area.onCodeMirrorLoad(cm);
+    }
+  };
+
+  function inject_css(uri, onload) {
+    var head = document.getElementsByTagName('head')[0],
+      style = document.createElement('link');
+    style.rel = 'stylesheet';
+    style.type = 'text/css';
+    style.href = uri;
+    style.onload = style.onreadystatechange = onload;
+    head.appendChild(style);
+  }
+
+  function cssOnload(id, callback) {
+    setTimeout(function listener(){
+      var el = document.getElementById(id),
+          comp = el.currentStyle || getComputedStyle(el, null);
+      if ( comp.height === "1px" )
+        callback();
+      else
+        setTimeout(listener, 50);
+    }, 50)
+  }
+
+  function bodyOnLoad(callback) {
+    setTimeout(function listener(){
+      if ( document.body )
+        callback();
+      else
+        setTimeout(listener, 50);
+    }, 50)
+  }
+
+  function convert_textareas() {
+    var areas = document.getElementsByClassName("codemirror-python");
+    for (var i = areas.length - 1; i >= 0; i--) {
+      convert_textarea(areas[i]);
+    };
+  }
+  bodyOnLoad(function() {
+    if (!window.CodeMirror) {
+      var sc = document.createElement('script');
+      sc.src = "/++resource++codemirror/codemirror-compressed.js";
+      document.body.appendChild(sc);
+    }
+    // Inject codemirror stylesheet in HEAD
+    var marker = document.createElement('div');
+    marker.id = 'CodeMirrorLoadedFlag';
+    document.body.appendChild(marker);
+    var base_codemirror_uri = "/++resource++codemirror";
+    inject_css(base_codemirror_uri + "/lib/codemirror.css");
+    inject_css(base_codemirror_uri + "/theme/elegant.css");
+    if (window.jQuery) {
+      // check if some textareas are good to be converted
+      jQuery("#plominoagent-base-edit #archetypes-fieldname-Content textarea").addClass('codemirror-python');
+    }
+    cssOnload('CodeMirrorLoadedFlag', convert_textareas);
+  });
+})();
